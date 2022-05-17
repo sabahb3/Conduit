176e8e3d47e794273a9502ddee4c1b918d1eb6b1
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 13:31:08 +0300

    Add an article class to encapsulate needed info about published articles like title, descriptions, and body

---
+++ Conduit.Domain/Articles.cs
@@ -0,0 +1,10 @@
+namespace Conduit.Domain;
+
+public class Articles
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Body { get; set; }
+    public DateTime Date { get; set; }
+}
