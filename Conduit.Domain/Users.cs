788f1f17d10399e382bc485126e2f3d771be33eb
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 14:31:11 +0300

    Add a collection navigation property of Articles type

--- Conduit.Domain/Users.cs
+++ Conduit.Domain/Users.cs
@@ -2,9 +2,15 @@ namespace Conduit.Domain;
 
 public class Users
 {
+    public Users()
+    {
+        Articles = new List<Articles>();
+    }
     public string username { get; set; }
     public string password { get; set; }
     public string Email { get; set; }
     public string? Bio { get; set; }
     public string ProfilePicture { get; set; }
+
+    public ICollection<Articles> Articles { get; set; }
 }
