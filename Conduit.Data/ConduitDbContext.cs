daca37f1b9273eb4cc887f590e38971b50a8cce3
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 16:45:34 +0300

    Add configurations to DbContext

--- Conduit.Data/ConduitDbContext.cs
+++ Conduit.Data/ConduitDbContext.cs
@@ -1,5 +1,6 @@
 using Conduit.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Conduit.Data;
 
@@ -9,4 +10,37 @@ public class ConduitDbContext : DbContext
     public DbSet<Articles> Articles { get; set; }
     public DbSet<Comments> Comments { get; set; }
     public DbSet<Tags> Tags { get; set; }
+    public DbContextOptionsBuilder ConduitOptions { get; private set; }
+
+
+    public ConduitDbContext()
+    {
+        
+    }
+
+    public ConduitDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+    public static readonly ILoggerFactory ConsoleLoggerFactory
+        = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+        });
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseLoggerFactory(ConsoleLoggerFactory).EnableSensitiveDataLogging()
+                .UseSqlServer(
+                    @"Server=localhost;
+                    Database=Conduit;
+                    Trusted_Connection=True;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        ConduitOptions = optionsBuilder;
+    }
 }
