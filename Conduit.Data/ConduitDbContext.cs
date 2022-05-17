76f42cc5d91eb199c36205c277c8a1520023a888
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 16:26:55 +0300

    Add needed DbSets

--- Conduit.Data/ConduitDbContext.cs
+++ Conduit.Data/ConduitDbContext.cs
@@ -5,5 +5,8 @@ namespace Conduit.Data;
 
 public class ConduitDbContext : DbContext
 {
-    
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Articles> Articles { get; set; }
+    public DbSet<Comments> Comments { get; set; }
+    public DbSet<Tags> Tags { get; set; }
 }
