cd10adf9e66f96b0d9d91536342f561c041fcfe7
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 15:32:23 +0300

    Add a collection navigation property of Followers type.

--- Conduit.Domain/Followers.cs
+++ Conduit.Domain/Followers.cs
@@ -4,4 +4,7 @@ public class Followers
 {
     public string Username { get; set; }
     public string FollowerId { get; set; }
+
+    public Users User { get; set; }
+    public Users Follower { get; set; }
 }
