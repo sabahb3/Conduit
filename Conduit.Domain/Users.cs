dea557f49f1f6733450c45a55cc8a34ad09fc8b9
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-17 13:23:53 +0300

    Add a user class to encapsulate needed info about users like username, email, password, bio, and profile picture.

---
+++ Conduit.Domain/Users.cs
@@ -0,0 +1,10 @@
+namespace Conduit.Domain;
+
+public class Users
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string Email { get; set; }
+    public string? Bio { get; set; }
+    public string ProfilePicture { get; set; }
+}
