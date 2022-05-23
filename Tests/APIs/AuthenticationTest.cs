f59c9cbff9fc536162c6cdb1695d6a955f2ffa37
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-23 16:22:23 +0300

    Write tests for signup status code

--- Tests/APIs/AuthenticationTest.cs
+++ Tests/APIs/AuthenticationTest.cs
@@ -1,11 +1,7 @@
 using System.Net;
 using System.Text;
-using AutoMapper;
-using Conduit.Data;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace Tests.APIs;
@@ -45,6 +41,66 @@ public class AuthenticationTest
         var content = new StringContent(objAsJson, Encoding.UTF8, "application/json");
 
         var uri = new Uri("https://localhost:7240/api/users/login");
+        var response = await client.PostAsync(uri, content);
+
+        response.StatusCode.Should().Be((HttpStatusCode)404);
+        
+    }
+    [Fact]
+    public async Task ShouldReturn200WhenSignup()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+        var user = new
+        {
+            email = "sabahB399@gmail.com",
+            password = "4050",
+            Username = "Sabahb3"
+        };
+        var objAsJson = JsonConvert.SerializeObject(user);
+        var content = new StringContent(objAsJson, Encoding.UTF8, "application/json");
+
+        var uri = new Uri("https://localhost:7240/api/users");
+        var response = await client.PostAsync(uri, content);
+
+        response.StatusCode.Should().Be((HttpStatusCode)200);
+        
+    }
+    [Fact]
+    public async Task ShouldReturn404WhenSignupWithExistingUsername()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+        var user = new
+        {
+            email = "sabahB399@gmail.com",
+            password = "4050",
+            Username = "sabah"
+        };
+        var objAsJson = JsonConvert.SerializeObject(user);
+        var content = new StringContent(objAsJson, Encoding.UTF8, "application/json");
+
+        var uri = new Uri("https://localhost:7240/api/users");
+        var response = await client.PostAsync(uri, content);
+
+        response.StatusCode.Should().Be((HttpStatusCode)404);
+        
+    }
+
+    [Fact] public async Task ShouldReturn404WhenSignupWithExistingEmail()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+        var user = new
+        {
+            email = "sabahBaara@gmail.com",
+            password = "4050",
+            Username = "sabahb3"
+        };
+        var objAsJson = JsonConvert.SerializeObject(user);
+        var content = new StringContent(objAsJson, Encoding.UTF8, "application/json");
+
+        var uri = new Uri("https://localhost:7240/api/users");
         var response = await client.PostAsync(uri, content);
 
         response.StatusCode.Should().Be((HttpStatusCode)404);
