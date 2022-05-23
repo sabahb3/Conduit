3134e44e551422ef6dd58a5dd2073c0817b82992
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-23 06:59:14 +0300

    Write the first version of the login where it returns 404 when there is no user, and 200 if exists.

--- Conduit.API/Controllers/AuthenticationController.cs
+++ Conduit.API/Controllers/AuthenticationController.cs
@@ -1,3 +1,6 @@
+using Conduit.API.Credentials;
+using Conduit.Data.IRepositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Conduit.API.Controllers;
@@ -6,5 +9,24 @@ namespace Conduit.API.Controllers;
 [Route("api/users")]
 public class AuthenticationController : ControllerBase
 {
-    
+    private readonly IConfiguration _configuration;
+    private readonly IUserRepository _userRepository;
+
+    public AuthenticationController(IConfiguration configuration, IUserRepository userRepository)
+    {
+        _configuration = configuration;
+        _userRepository = userRepository;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginCredentials loginCredentials)
+    {
+        var user = await _userRepository.CheckUser(loginCredentials.Email, loginCredentials.Password);
+        if (user != null)
+        {
+            return Ok();
+        }
+        return NotFound("User not found");
+    }
 }
