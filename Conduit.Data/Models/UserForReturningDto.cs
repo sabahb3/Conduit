27710cce054f7cd49739c5fc1efee832b9473337
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-24 10:41:53 +0300

    Declare a model for user to return it.

---
+++ Conduit.Data/Models/UserForReturningDto.cs
@@ -0,0 +1,10 @@
+namespace Conduit.Data.Models;
+
+public class UserForReturningDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+}
