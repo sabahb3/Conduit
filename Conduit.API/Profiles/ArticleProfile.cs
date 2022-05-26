ece52ce4a1617be0928be05cff424c02cbb05594
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-26 12:18:48 +0300

    Convert the constructor to a parameterless constructor.

--- Conduit.API/Profiles/ArticleProfile.cs
+++ Conduit.API/Profiles/ArticleProfile.cs
@@ -7,19 +7,12 @@ namespace Conduit.API.Profiles;
 
 public class ArticleProfile : Profile
 {
-    private readonly IArticleRepository _articleRepository;
 
-    public ArticleProfile(IArticleRepository articleRepository, ITagRepository tagRepository)
+    public ArticleProfile()
     {
-        _articleRepository = articleRepository;
         CreateMap<Articles, ArticleToReturnDto>().ForMember(
             des=>des.Slug,
             opt=>opt.MapFrom(src=>src.Title)
-            )
-            .ForMember(des=>des.FavoritesCount,
-                opt=>
-                    opt.MapFrom(src=>_articleRepository.CountWhoFavoriteArticle(src.Id))
-                )
-            ;
+            );
     }
 }
