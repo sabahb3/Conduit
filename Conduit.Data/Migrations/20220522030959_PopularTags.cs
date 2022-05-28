3727f56b5f72c087e1a31ffc7fca4976fe1b820a
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-28 07:20:56 +0300

    Add a new column as the updated date to the article table

---
+++ Conduit.Data/Migrations/20220522030959_PopularTags.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conduit.Data.Migrations
+{
+    public partial class PopularTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE FUNCTION PopularTags() 
+                                    RETURNS TABLE  
+                                    AS  
+                                    RETURN  
+                                        SELECT  TOP 4 Tag
+                                        FROM ArticlesTags
+                                        GROUP BY Tag
+                                        ORDER BY COUNT(Tag) DESC");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP FUNCTION IF EXISTS dbo.PopularTags;");
+        }
+    }
+}
---
+++ Conduit.Data/Migrations/20220528042001_AddUpdatedDateColumn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conduit.Data.Migrations
+{
+    public partial class AddUpdatedDateColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatingDate",
+                table: "Articles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UpdatingDate",
+                value: new DateTime(2022, 4, 20, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UpdatingDate",
+                value: new DateTime(2022, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "UpdatingDate",
+                value: new DateTime(2022, 2, 22, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatingDate",
+                table: "Articles");
+        }
+    }
+}
