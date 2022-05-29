2b30a31c7c271c24b6144596c1cad454f6eb03e1
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-05-29 23:15:33 +0300

    Add a model for updating an article

---
+++ Conduit.Data/Models/ArticleForUpdatingDto.cs
@@ -0,0 +1,8 @@
+namespace Conduit.Data.Models;
+
+public class ArticleForUpdatingDto
+{
+    public string? Title { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public string? Body { get; set; } = string.Empty;
+}
