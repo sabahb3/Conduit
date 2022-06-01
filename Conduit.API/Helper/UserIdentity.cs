8be666d13b421c0f12e2fabfd3a3d0b209217906
Sabah Ba'ara <sabahbaara4@gmail.com>
2022-06-01 05:25:00 +0300

    Own a new version of getting logged user to check if logout was performed or not.

--- Conduit.API/Helper/UserIdentity.cs
+++ Conduit.API/Helper/UserIdentity.cs
@@ -1,9 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Principal;
 using AutoMapper;
 using Conduit.Data.IRepositories;
 using Conduit.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
 
 namespace Conduit.API.Helper;
 
@@ -11,11 +13,13 @@ public class UserIdentity : ControllerBase
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IConnectionMultiplexer _redis;
 
-    public UserIdentity(IUserRepository userRepository,IMapper mapper)
+    public UserIdentity(IUserRepository userRepository,IMapper mapper,IConnectionMultiplexer redis)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _redis = redis;
     }
 
     public string? GetLoggedUser(IIdentity? userIdentity)
@@ -23,6 +27,12 @@ public class UserIdentity : ControllerBase
         var identity = userIdentity as ClaimsIdentity;
         return identity == null ? 
             null : identity.Claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value;
+    }
+    public async Task<string?> GetLoggedUser(IIdentity? userIdentity,string? token)
+    {
+        if (userIdentity is not ClaimsIdentity identity) return null;
+        var isLogout = await CheckLogout(token);
+        return isLogout is null or true ? null : identity.Claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value;
     }
     public async Task<ProfileDto?> PrepareProfile(IIdentity? identity,string username)
     {
@@ -42,5 +52,16 @@ public class UserIdentity : ControllerBase
         username = username.Trim();
         var user = await _userRepository.GetUser(username);
         return user != null;
+    }
+
+    public async Task<bool?> CheckLogout(string? tokenString)
+    {
+        if (tokenString == null) return null;
+        var token = new JwtSecurityToken(tokenString);
+        var username = token.Claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)!.Value;
+        var expired =  token.Claims.FirstOrDefault(o => o.Type == "exp")!.Value;
+        var isLogout =await _redis.GetDatabase().StringGetAsync(username + expired);
+        if (isLogout == RedisValue.Null) return false;
+        return true;
     }
 }
